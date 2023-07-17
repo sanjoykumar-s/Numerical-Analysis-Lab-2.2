b1a1fb99f9550fe0cc5cef407ac573632a024ef5
Sanjoy Kumar <sanjoy.ru.cse@gmail.com>
2023-07-17 23:33:36 +0600

    integrationUsingTrapezoidalRule.cpp is complete.\n

--- integrationUsingTrapezoidalRule.cpp
+++ integrationUsingTrapezoidalRule.cpp
@@ -1,11 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void integrationUsingTrapezoidal Rules() {
+double f(double x) {
+    return 1/(1+x);
+}
+
+void integrationUsingTrapezoidal() {
+    cout << "Enter lower Bound:\n";
+    double a;
+    cin >> a;
+    double b;
+    cout << "Enter upper bound :\n";
+    cin >> b;
+    int n;
+    cout << "Enter number of interval:\n";
+    cin >> n;
+    double h = (b-a)/n;
+    vector<double>v;
+    double res = 0;
+    v.push_back(f(a));
+    res+=v[0];
+    double tem = a;
+    while(tem+h<b) {
+        tem+=h;
+        res+=2*f(tem);
+    } 
+    res+=f(b);
+    res*=h/2.0;
+    cout << "Integrated value = " << res << endl;
 
 }
 
 int32_t main() {
-    GaussElimination();
+    integrationUsingTrapezoidal();
     return 0;
 }
